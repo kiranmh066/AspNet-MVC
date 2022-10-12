65696bf7649466f89a26986119c5cb9367dc8213
Kiran Mahalingappa Hegganadavara <Kiran.MH@valtech.com>
2022-10-12 18:20:34 +0530

    commit the AspNet MVC

---
+++ BookShow/MovieCoreMvcUI/Controllers/MovieController.cs
@@ -0,0 +1,57 @@
+﻿using BookShowEntity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Ini;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCoreMvcUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public MovieController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MovieEntry()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> MovieEntry(Movie movie)
+        {
+            ViewBag.status = "";
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(movie),Encoding.UTF8,"application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "Movie/AddMovie";
+                using (var response = await client.PostAsync(endPoint, content))
+                {
+                    if(response.StatusCode==System.Net.HttpStatusCode.OK)
+                    {
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "Movie deatils Saved Successfully";
+                    }
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong Entries....!!!  :)";
+                    }
+                }
+            }
+
+            return View();
+        }
+
+
+    }
+}
